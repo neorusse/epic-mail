11231794c679bee7814494d78aed0ff37214561c
Russell Nyorere <ecodenhub@gmail.com>
2019-03-07 10:05:28 +0100

    [Bug #164456067] Add mock data as db

---
+++ server/db/users.js
@@ -0,0 +1,12 @@
+const Users = [
+  {
+    id: 1,
+    firstName: 'Russell',
+    lastName: 'Nyorere',
+    email: 'russ@gmail.com',
+    password: 'internet12',
+    mobileNumber: '08068908852'
+  }
+];
+
+export default Users;
